914df5a695c176ade34ee98e14c25b4b4bb81825
vintage254 <derricknjuguna414@gmail.com>
2024-01-04 04:48:00 -0800

    testing3

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import calculator_1 as calc
-    a = 10 
+    a = 10
     b = 5
     print ("{} + {} = {}".format(a, b, calc.add(a, b)))
     print ("{} - {} = {}".format(a, b, calc.sub(a, b)))
