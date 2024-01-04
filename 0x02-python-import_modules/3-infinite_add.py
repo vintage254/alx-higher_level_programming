7cf0b588378fd37d4b617abd3c948938e42d11b0
vintage254 <derricknjuguna414@gmail.com>
2024-01-04 06:26:07 -0800

    advanced

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     from sys import argv
     sumof = 0
     for i in range (1, len(argv)):
-    sumof += int(argv[i])
+        sumof += int(argv[i])
     print ("{}".format(sumof))
