71db39fc5c5c6ea6e606bdd2e9d4720c5c73702e
vintage254 <derricknjuguna414@gmail.com>
2024-02-11 05:58:17 -0800

    diplay height using #

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -68,3 +68,9 @@ class Rectangle(Base):
     def area(self):
         """Return the area of the Rectangle."""
         return self.width * self.height
+    def display(self):
+        """
+        prints size of the rectangle using #
+        """
+        for _ in range(self.height):
+            print("#" * self.width)
