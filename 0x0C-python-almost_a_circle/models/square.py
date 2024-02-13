effa5d234edcd2854516bbe8dbd3ce7dc1a45895
vintage254 <derricknjuguna414@gmail.com>
2024-02-13 05:26:10 -0800

    10

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,6 +10,20 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id= None):
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """
+        size getter
+        """
+        return self.width
+    @size.setter
+    def size(self ,value):
+        """
+        size setter
+        """
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """
         string reprasantation
