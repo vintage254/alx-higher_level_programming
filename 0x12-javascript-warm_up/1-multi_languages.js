50b00d819e5be9f64af4ae74e8343c13798ac406
vintage254 <derricknjuguna414@gmail.com>
2024-04-08 08:22:30 -0700

    fix

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-const print = [ 'C is fun', 'Python is cool', 'JavaScript is amazing' ];
-for (let x in print)
-{
-	console.log(print[x]);
+const print = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const x in print) {
+  console.log(print[x]);
 }
