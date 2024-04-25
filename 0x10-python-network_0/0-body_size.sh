e5c8fb857eb962e6dd6694f227a4c720215e30df
“vintage254” <derricknjuguna414@gmail.com>
2024-04-25 13:59:58 -0700

    task0 fix 2

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
 # script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-
-response=$(curl -s -o /dev/null -w "%{size_download}" "$1")
-response_bit=$((response * 8))
-echo "$response_bit"
+response=$(curl -s -o /dev/null -w "%{size_download}" "$1"); response_bit=$((response * 8)); echo "$response_bit"
