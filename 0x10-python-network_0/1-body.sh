39439919cbcfaba46cbe2b649c93d88c7197b146
“vintage254” <derricknjuguna414@gmail.com>
2024-04-25 14:16:56 -0700

    task 1 fix up

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 #script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-response=$(curl -s -w "%{http_code}" "$1"); if [ "$response" == "200" ]; then echo "$response"; else echo "Received HTTP status code $response. Body not displayed."; fi
-
+response=$(curl -s -w "%{http_code}" "$1"); if [ "$response" == "200" ]; then echo "$response"; else echo "Received HTTP status code $response. Body not displayed."; fi/n
