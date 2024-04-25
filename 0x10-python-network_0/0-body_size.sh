4a09a4eabd4f2b642df05ed5f3343ee62037f54f
“vintage254” <derricknjuguna414@gmail.com>
2024-04-25 12:47:26 -0700

    fixes

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-response=curl -s -o /dev/null -w "%{size_download}" <URL>
-printf "response_size"
+
+# Check if the number of arguments is not equal to 1
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+# Assign the URL passed as an argument to the variable
+url="$1"
+
+# Use curl to send a request to the URL and print the size of the response body
+response=$(curl -s -o /dev/null -w "%{size_download}" "$url")
+
+# Print the size of the response body
+echo "$response"
+
