f2f67bbb9002f0054c54d617fb76677e5198618e
“vintage254” <derricknjuguna414@gmail.com>
2024-04-25 13:47:59 -0700

    task0 fix 2

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-# Check if any URL is provided as argument
-if [ "$#" -eq 0 ]; then
-    echo "Usage: $0 <URL1> [<URL2> ...]"
-    exit 1
-fi
+# script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
 
-# Iterate over each URL argument
-for url in "$@"; do
-    # Send a GET request to the URL and display the size of the body of the response in bits
-    response=$(curl -s -w "%{size_download}" "$url" | awk '{ printf "%.0f", $1 * 8 }')
-    echo "$response"
-done
+response_bit=$(curl -s -w "%{size_download}" "$1" | awk '{ printf "%.0f", $1 * 8 }')
+echo "$response_bit"
 
