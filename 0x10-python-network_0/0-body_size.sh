28d44a60f85d93455757c4778b6c8ffe1ce890f8
“vintage254” <derricknjuguna414@gmail.com>
2024-04-25 13:50:10 -0700

    task0 fix 2

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,6 +2,7 @@
 
 # script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
 
-response_bit=$(curl -s -w "%{size_download}" "$1" | awk '{ printf "%.0f", $1 * 8 }')
+response=$(curl -s -w "%{size_download}" "$1")
+response_bit=$((response * 8))
 echo "$response_bit"
 
