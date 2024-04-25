f637f0924caf3c8d7fdee279faade478864fccb8
“vintage254” <derricknjuguna414@gmail.com>
2024-04-25 13:52:12 -0700

    task0 fix 2

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if [ -z "$1" ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
 
 # script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
 
