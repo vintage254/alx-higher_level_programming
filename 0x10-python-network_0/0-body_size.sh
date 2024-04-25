33800caff8cd45e870dd6922acc98db22daeaaae
“vintage254” <derricknjuguna414@gmail.com>
2024-04-25 14:15:02 -0700

    task 1 fix up

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
 response=$(curl -s -o /dev/null -w "%{size_download}" "$1"); response_bit=$((response * 8)); echo "$response_bit"
+
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
+#script that takes in a URL, sends a GET request to the URL, and displays the body of the response
+response=$(curl -s -w "%{http_code}" "$1"); if [ "$response" == "200" ]; then echo "$response"; else echo "Received HTTP status code $response. Body not displayed."; fi
 
-# Check if URL is provided as argument
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-response=$(curl -s -w "%{http_code}" "$1")
-
-if [ "$response" == "200" ]; then
-    echo "$response"
-else
-    echo "Received HTTP status code $response. Body not displayed."
-fi
