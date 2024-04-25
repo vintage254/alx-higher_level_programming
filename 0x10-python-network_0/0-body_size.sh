c1faec862a7eb20bd87c8c73fda2ea111d824120
“vintage254” <derricknjuguna414@gmail.com>
2024-04-25 13:05:12 -0700

    bit fix

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
-
-# Check if the number of arguments is not equal to 1
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Assign the URL passed as an argument to the variable
-url="$1"
-
 # Use curl to send a request to the URL and print the size of the response body
-response=$(curl -s -o /dev/null -w "%{size_download}" "$url")
 
+response=$(curl -s -o /dev/null -w "%{size_download}" "$1")
+response_bits=$((response * 8))
 # Print the size of the response body
-echo "$response"
+echo "$response_bits"
 
