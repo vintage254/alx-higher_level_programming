0a895f10e9239ba04dd1501dea927ee31ec24f2d
“vintage254” <derricknjuguna414@gmail.com>
2024-04-25 13:31:52 -0700

    task0 fix

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -6,7 +6,7 @@ if [ -z "$1" ]; then
 fi
 # Use curl to send a request to the URL and print the size of the response body
 
-response=$(curl -s -o /dev/null -w "%{size_download}" "$1")
+response=$(curl -s -w "%{size_download}" "$1")
 response_bits=$((response * 8))
 # Print the size of the response body
 echo "$response_bits"
