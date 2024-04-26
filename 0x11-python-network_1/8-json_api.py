6cf60c1345270cbf9f5b565aedeb139a6279b210
“vintage254” <derricknjuguna414@gmail.com>
2024-04-26 09:53:24 -0700

    Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+
+"""This script sends a POST request to http://0.0.0.0:5000/search_user
+with a specified letter as a parameter and displays the response."""
+
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        q = ""
+    else:
+        q = sys.argv[1]
+
+    url = 'http://0.0.0.0:5000/search_user'
+    data = {'q': q}
+
+    response = requests.post(url, data=data)
+
+    try:
+        json_response = response.json()
+        if json_response:
+            print("[{}] {}".format(json_response['id'], json_response['name']))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
